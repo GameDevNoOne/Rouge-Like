df0b7d175d69abc17ff10954b7732fe08eda47e5
GameDevNoOne <zlati.volk@gmail.com>
2023-03-20 08:38:13 +0100

    Started UI

---
+++ Rouge-LikeCubeWorld/Assets/Scripts/PlayerScripts/CoinCounter.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinCounter : MonoBehaviour
+{
+    [SerializeField] public Text coinCounter;
+    private float Money;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Money = GetComponent<PlayerActions>().Money;
+        string MoneyOut = Money.ToString();
+        coinCounter.text = (MoneyOut);
+    }
+}
--- Rouge-LikeCubeWorld/Assets/Scripts/PlayerScripts/PlayerStats.cs
+++ Rouge-LikeCubeWorld/Assets/Scripts/PlayerScripts/PlayerStats.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
     [SerializeField] public float Health;
     [SerializeField] public GameObject weapon;
+    [SerializeField] private float Money;
+
+    private void Update()
+    {
+        Money = GetComponent<PlayerActions>().Money;
+    }
 }
