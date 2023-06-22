bdbb6edcaa148757dcb7806d89e6bb74ccd868c8
GameDevNoOne <zlati.volk@gmail.com>
2023-06-22 11:22:50 +0200

    It works!!! It's ALIVE!!!!!
    The dungeon generator works perfectly, all I need to do now is spawn a boss at the end and make it all look better as well as add enemy spawners and loot spawners

--- Rouge-LikeCubeWorld/Assets/Scripts/RoomSpawner.cs
+++ Rouge-LikeCubeWorld/Assets/Scripts/RoomSpawner.cs
@@ -7,7 +7,7 @@ public class RoomSpawner : MonoBehaviour
     public int openingDir;
     private RoomTemplate roomTemplate;
     private int random;
-    private bool Spawned = false;
+    public bool Spawned = false;
 
     public void Start()
     {
@@ -21,23 +21,23 @@ public class RoomSpawner : MonoBehaviour
         {
             if (openingDir == 1)
             {
-                random = Random.Range(0, roomTemplate.topRooms.Length);
-                Instantiate(roomTemplate.topRooms[random], transform.position, roomTemplate.topRooms[random].transform.rotation);
-            }
-            else if (openingDir == 2)
-            {
-                random = Random.Range(0, roomTemplate.rightRooms.Length);
-                Instantiate(roomTemplate.rightRooms[random], transform.position, roomTemplate.rightRooms[random].transform.rotation);
-            }
-            else if (openingDir == 3)
-            {
                 random = Random.Range(0, roomTemplate.bottomRooms.Length);
                 Instantiate(roomTemplate.bottomRooms[random], transform.position, roomTemplate.bottomRooms[random].transform.rotation);
             }
-            else if (openingDir == 4)
+            else if (openingDir == 2)
             {
                 random = Random.Range(0, roomTemplate.leftRooms.Length);
                 Instantiate(roomTemplate.leftRooms[random], transform.position, roomTemplate.leftRooms[random].transform.rotation);
+            }
+            else if (openingDir == 3)
+            {
+                random = Random.Range(0, roomTemplate.topRooms.Length);
+                Instantiate(roomTemplate.topRooms[random], transform.position, roomTemplate.topRooms[random].transform.rotation);
+            }
+            else if (openingDir == 4)
+            {
+                random = Random.Range(0, roomTemplate.rightRooms.Length);
+                Instantiate(roomTemplate.rightRooms[random], transform.position, roomTemplate.rightRooms[random].transform.rotation);
             }
 
             Spawned = true;
@@ -46,7 +46,7 @@ public class RoomSpawner : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("SpawnPoint") && collision.gameObject.GetComponent<RoomSpawner>().Spawned == true)
+        if (collision.CompareTag("SpawnPoint"))
         {
             Destroy(gameObject);
         }
