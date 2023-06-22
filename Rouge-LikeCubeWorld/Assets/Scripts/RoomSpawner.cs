f497a9b1972b3ef2108604f54cbe4238a543a163
GameDevNoOne <zlati.volk@gmail.com>
2023-06-22 09:00:58 +0200

    Random Dungeon Generator
    It kind of works except that it crashes my computer if I try to run it. I'll have to look into that.

--- Rouge-LikeCubeWorld/Assets/Scripts/RoomSpawner.cs
+++ Rouge-LikeCubeWorld/Assets/Scripts/RoomSpawner.cs
@@ -5,24 +5,50 @@ using UnityEngine;
 public class RoomSpawner : MonoBehaviour
 {
     public int openingDir;
+    private RoomTemplate roomTemplate;
+    private int random;
+    private bool Spawned = false;
 
-    public void Update()
+    public void Start()
     {
-        if (openingDir == 1)
+        roomTemplate = GameObject.FindGameObjectWithTag("Room").GetComponent<RoomTemplate>();
+        Invoke("Spawn", 0.1f);
+    }
+
+    public void Spawn()
+    {
+        if (Spawned == false)
         {
-            //BottomDoor
+            if (openingDir == 1)
+            {
+                random = Random.Range(0, roomTemplate.topRooms.Length);
+                Instantiate(roomTemplate.topRooms[random], transform.position, roomTemplate.topRooms[random].transform.rotation);
+            }
+            else if (openingDir == 2)
+            {
+                random = Random.Range(0, roomTemplate.rightRooms.Length);
+                Instantiate(roomTemplate.rightRooms[random], transform.position, roomTemplate.rightRooms[random].transform.rotation);
+            }
+            else if (openingDir == 3)
+            {
+                random = Random.Range(0, roomTemplate.bottomRooms.Length);
+                Instantiate(roomTemplate.bottomRooms[random], transform.position, roomTemplate.bottomRooms[random].transform.rotation);
+            }
+            else if (openingDir == 4)
+            {
+                random = Random.Range(0, roomTemplate.leftRooms.Length);
+                Instantiate(roomTemplate.leftRooms[random], transform.position, roomTemplate.leftRooms[random].transform.rotation);
+            }
+
+            Spawned = true;
         }
-        else if (openingDir == 2)
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("SpawnPoint") && collision.gameObject.GetComponent<RoomSpawner>().Spawned == true)
         {
-            //LeftDoor
-        }
-        else if (openingDir == 3)
-        {
-            //UpDoor
-        }
-        else if (openingDir == 4)
-        {
-            //RightDoor
+            Destroy(gameObject);
         }
     }
 }
